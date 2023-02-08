538fa9eef4b070c07fbeedfd0af2ef1ba707c7c4
Danut Mirila <vasilemirila@gmail.com>
2023-02-08 11:05:06 +0200

    device type

--- src/index.js
+++ src/index.js
@@ -348,6 +348,20 @@ app.get('/find/workIndustry', (req, res) => {
   });
 });
 
+app.post('/device', (req, res) => {
+  try {
+    if ( req.body ) {
+      console.log(req.body);
+      res.status(200).json({message: 'received type'});
+      console.log('received type');
+    } else {
+      res.status(500).json({message: "Something went wrong!"});
+    }
+  } catch (error) {
+    res.status(500).json({message: "Something went wrong!!"});
+  }
+})
+
 app.use(notFound);
 app.use(errorHandler);
 
